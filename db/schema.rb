d6559ddec5876fb18a5a1b6abae84ce1a7955390
Patriarh22 <kvs2204@gmail.com>
2020-03-21 18:50:11 +0200

    Added handle to Post model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_21_143709) do
+ActiveRecord::Schema.define(version: 2020_03_21_155123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2020_03_21_143709) do
     t.integer "source_id"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
+    t.string "handle"
   end
 
   create_table "sources", force: :cascade do |t|
