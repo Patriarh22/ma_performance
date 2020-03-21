58c5fc98ee4e75e8f2a94306685850b8357cebfd
Patriarh22 <kvs2204@gmail.com>
2020-03-21 16:36:11 +0200

    Added Post model and posts table in DB

---
+++ app/models/post.rb
@@ -0,0 +1,15 @@
+# == Schema Information
+# Schema version: 20200321142546
+#
+# Table name: posts
+#
+#  id         :bigint           not null, primary key
+#  title      :string
+#  body       :text
+#  author     :string
+#  source_id  :integer
+#  created_at :datetime         not null
+#  updated_at :datetime         not null
+#
+class Post < ApplicationRecord
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_03_15_185934) do
+ActiveRecord::Schema.define(version: 2020_03_21_142546) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "posts", force: :cascade do |t|
+    t.string "title"
+    t.text "body"
+    t.string "author"
+    t.integer "source_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "sources", force: :cascade do |t|
     t.string "name"
