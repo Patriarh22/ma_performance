dde49f7610d686538def07fb1c4530e0757f3b4b
Patriarh22 <kvs2204@gmail.com>
2020-03-25 22:39:06 +0200

    Added routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,11 @@
 Rails.application.routes.draw do
-  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  root to: 'sources#index'
+
+  resources :sources, only: :index do
+    member do
+      get :sync_posts
+      get :sync_comments
+    end
+    resources :posts, only: :show
+  end
 end
