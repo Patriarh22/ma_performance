3ce0e74b59f8c1d78d5a6f653cd66905ff7cf0df
Patriarh22 <kvs2204@gmail.com>
2020-03-25 23:29:09 +0200

    Logic for sync_posts method of SourcesController

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -1,14 +1,25 @@
 class SourcesController < ApplicationController
+  before_action :find_source, only: %i[sync_posts sync_comments]
+
   def index
     @title = 'Sources'
     @sources = Source.all
   end
 
   def sync_posts
+    unless @source&.sync_posts
+      flash[:error] = "Something went wrong"
+    end
     redirect_to root_path
   end
 
   def sync_comments
     redirect_to root_path
+  end
+
+  private
+
+  def find_source
+    @source = Source.find_by(id: params[:id])
   end
 end
