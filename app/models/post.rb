fde2bae56dc57530463cb760f753bc7759ece1a3
Patriarh22 <kvs2204@gmail.com>
2020-03-26 23:14:11 +0200

    Logic for sync_comments method in Post model

--- app/models/post.rb
+++ app/models/post.rb
@@ -13,6 +13,21 @@
 #  handle     :string
 #
 class Post < ApplicationRecord
-  has_many :comments
+  has_many :comments, dependent: :destroy
   belongs_to :source
+
+  def sync_comments(source_connector = nil)
+    source_connector ||= source.connector_instance
+    return false unless source_connector
+    comments.destroy_all
+    post_comments = source_connector.fetch_post_comments(handle)
+    post_comments.map(&method(:create_single_comment)).all?(&:present?)
+  end
+
+  private
+
+  def create_single_comment(comment_data)
+    comment = comments.new(comment_data)
+    comment.save
+  end
 end
