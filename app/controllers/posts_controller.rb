2a04d8717bac3d08488314fa6674e5e9f33e7610
Patriarh22 <kvs2204@gmail.com>
2020-03-28 19:56:29 +0200

    Added action sync_comments to PostsController

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,4 +1,22 @@
 class PostsController < ApplicationController
+  before_action :find_post
+
   def show
+    redirect_to_root_path(false) unless @post
+  end
+
+  def sync_comments
+    if @post
+      @post.sync_comments
+      redirect_to source_post_path(@post.source_id, @post)
+    else
+      redirect_to_root_path(false)
+    end
+  end
+
+  private
+
+  def find_post
+    @post = Post.find_by(id: params[:id], source_id: params[:source_id])
   end
 end
