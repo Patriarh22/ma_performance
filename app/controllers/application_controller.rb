cd1a7792729fa5305d854b70cefdcf58aa971963
Patriarh22 <kvs2204@gmail.com>
2020-03-28 19:55:41 +0200

    Moved redirect_to_root_path method to ApplicationController

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::Base
+  private
+
+  def redirect_to_root_path(success)
+    unless success
+      flash[:error] = "Something went wrong"
+    end
+    redirect_to root_path
+  end
 end
