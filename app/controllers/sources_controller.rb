f31f00891407fd2ab40acad9e3b7bcf4b51aed55
Patriarh22 <kvs2204@gmail.com>
2020-03-31 14:40:27 +0300

    Added ordering to sources list

--- app/controllers/sources_controller.rb
+++ app/controllers/sources_controller.rb
@@ -3,7 +3,7 @@ class SourcesController < ApplicationController
 
   def index
     @title = 'Sources'
-    @sources = Source.all
+    @sources = Source.order(created_at: :asc).all
   end
 
   def sync_posts
